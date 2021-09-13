8930cafb4fc6217ecf5672d0d5ce2352490a8e73
donRumata <donrumata03@gmail.com>
2021-09-13 22:28:45 +0300

    Small refactor (change one variable case)

--- HT-2/RawSolution/src/Sum.java
+++ HT-2/RawSolution/src/Sum.java
@@ -6,16 +6,16 @@ public class Sum {
             boolean inNumber = false;
             int numberStartIndex = 0;
 
-            for (int char_index = 0; char_index <= arg.length(); char_index++) {
-                if (inNumber && (char_index == arg.length() || Character.isWhitespace(arg.charAt(char_index)))) {
+            for (int charIndex = 0; charIndex <= arg.length(); charIndex++) {
+                if (inNumber && (charIndex == arg.length() || Character.isWhitespace(arg.charAt(charIndex)))) {
                     // Handle number end:
-                    totalSum += Integer.parseInt(arg.substring(numberStartIndex, char_index));
+                    totalSum += Integer.parseInt(arg.substring(numberStartIndex, charIndex));
 
                     inNumber = false;
-                } else if (!inNumber && char_index != arg.length() && !Character.isWhitespace(arg.charAt(char_index))) {
+                } else if (!inNumber && charIndex != arg.length() && !Character.isWhitespace(arg.charAt(charIndex))) {
                     // Handle number start:
                     inNumber = true;
-                    numberStartIndex = char_index;
+                    numberStartIndex = charIndex;
                 }
             }
         }
