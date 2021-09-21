f6a0ee311ff673c3af7ed8a9d571d57f061f46a1
donRumata03 <donrumata03@gmail.com>
2021-09-21 20:08:55 +0300

    Finish HT-3

--- HT-3/src/ReverseSum2.java
+++ HT-3/src/ReverseSum2.java
@@ -1,30 +1,72 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
-public class Reverse {
-    public static void main(String[] args) {
-        Scanner inputReader = new Scanner(System.in);
+import java.lang.Math;
 
-        List<String> inputData = new ArrayList<String>();
-        while(inputReader.hasNextLine()) {
-            inputData.add(inputReader.nextLine());
+
+class FuckingJavaResizableIntArrayWithoutOnMetadataFuckingJavaWhyAreThereNoPrimitiveTypeTemplates {
+    // not extends List<Integer>
+
+    private int mSize;
+    private int[] mArray;
+
+    FuckingJavaResizableIntArrayWithoutOnMetadataFuckingJavaWhyAreThereNoPrimitiveTypeTemplates() {
+        mSize = 0;
+        mArray = new int[1];
+    }
+
+    public int size() {
+        return mSize;
+    }
+
+    private void reallocate(int newCapacity) {
+        mArray = Arrays.copyOf(mArray, newCapacity);
+    }
+
+    public void add(int element) {
+        if (mSize + 1 == mArray.length) {
+            reallocate(Math.max(mArray.length * 2, 4));
         }
+        mArray[mSize++] = element;
+    }
+    public int get(int index) {
+        return mArray[index];
+    }
+    public void set(int index, int element) {
+        mArray[index] = element;
+    }
+}
 
-        Collections.reverse(inputData);
-        for (String line: inputData) {
-            Scanner lineParser = new Scanner(line);
-            List<Integer> thisNumbers = new ArrayList<Integer>();
-            while (lineParser.hasNext()) {
-                thisNumbers.add(lineParser.nextInt());
+
+public class ReverseSum2 {
+    public static void main(String[] args) {
+        FuckingJavaResizableIntArrayWithoutOnMetadataFuckingJavaWhyAreThereNoPrimitiveTypeTemplates
+                prefix2dSum =
+                new FuckingJavaResizableIntArrayWithoutOnMetadataFuckingJavaWhyAreThereNoPrimitiveTypeTemplates();
+
+        Scanner inputReader = new Scanner(System.in);
+        while(inputReader.hasNextLine()) {
+            Scanner lineParser = new Scanner(inputReader.nextLine());
+
+            int columnIndex = 0;
+            int matrixSum = 0;
+            while (lineParser.hasNextInt()) {
+                int thisElement = lineParser.nextInt();
+
+                if (columnIndex < prefix2dSum.size()) {
+                    prefix2dSum.set(columnIndex, prefix2dSum.get(columnIndex) + thisElement);
+                } else {
+                    prefix2dSum.add(thisElement);
+                }
+                matrixSum += prefix2dSum.get(columnIndex);
+
+                if (columnIndex != 0) {
+                    System.out.print(" ");
+                }
+                System.out.print(matrixSum);
+
+                columnIndex++;
             }
-            Collections.reverse(thisNumbers);
-            ArrayList<String> stringifiedNumbers = new ArrayList<String>();
-            for (Integer integer : thisNumbers) {
-                stringifiedNumbers.add(integer.toString());
-            }
-            System.out.println(String.join(" ", stringifiedNumbers));
+            System.out.println();
         }
     }
 }
