14b6959d9c786edb5b2f7ba6322b80cde3d4e7b0
donRumata <donrumata03@gmail.com>
2021-10-12 11:10:07 +0300

    Use custom scanner for HT-4 and fix small problems there

---
+++ HT-4/src/BufferedScanner.java
@@ -0,0 +1,105 @@
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.NoSuchElementException;
+import java.util.function.IntPredicate;
+
+public class BufferedScanner implements Closeable, AutoCloseable {
+    private static boolean isFromWord(int c) {
+        return Character.isLetter(c) || Character.getType(c) == Character.DASH_PUNCTUATION || (char)c == '\'';
+    }
+
+    private static final char LF = 0x000A;
+    private static final char CR = 0x000D;
+    private static final char[] lineSeparators = { LF, 0x000B, 0x000C, CR, 0x0085, 0x2028, 0x2029 };
+
+    private static boolean isLineSeparator(int c) {
+        for (char sep : lineSeparators) {
+            if (sep == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    ReaderBufferizer in;
+    int read = -1;
+
+    public BufferedScanner (Reader reader) {
+        in = new ReaderBufferizer(reader);
+    }
+    public BufferedScanner (Reader reader, int bufferSize) {
+        in = new ReaderBufferizer(reader, bufferSize);
+    }
+
+
+
+    public String nextSequence(IntPredicate isDelimiter) throws IOException {
+        if (!in.hasNextChar()) {
+            // throw new NoSuchElementException("EndOfStream: There are no sequences to read");
+            return null;
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        while (in.hasNextChar()) {
+            read = in.nextChar();
+            if (isDelimiter.test(read)) {
+                break;
+            }
+            builder.append((char)read);
+        }
+        return builder.toString();
+    }
+
+    public String nextSequenceIgnoreEmpty(IntPredicate isDelimiter) throws IOException {
+        // Skip delimiters
+        // (may be drastically faster than calling nextSequence every time while it doesn't become empty
+        // for files with a lot of spaces)
+        while (in.hasNextChar() && in.testNext(isDelimiter)) {
+            read = in.nextChar();
+        }
+
+        // Read is now first character in sequence or EOS
+        return nextSequence(isDelimiter);
+    }
+//    public String nextSequence(IntPredicate isDelimiter) { return nextSequence(isDelimiter, false); }
+
+
+
+    public Integer nextInt() throws IOException, NoSuchElementException, NumberFormatException {
+        String token = nextSequenceIgnoreEmpty(Character::isWhitespace);
+        if (token == null) {
+             throw new NoSuchElementException("EndOfStream: There are no integers to read");
+        }
+        return Integer.parseInt(token);
+    }
+
+    public String nextLine() throws IOException {
+        String lineAttempt = nextSequence(BufferedScanner::isLineSeparator);
+
+        if (lineAttempt != null) {
+            if (read == CR && in.hasNextChar() && in.testNext((int ch) -> ch == LF)) {
+                in.nextChar();
+            }
+            return lineAttempt;
+        }
+        return null;
+    }
+
+
+    public String nextWord() throws IOException {
+        try {
+            return nextSequenceIgnoreEmpty((int ch) -> !BufferedScanner.isFromWord(ch));
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+    }
+
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+    }
+}
--- HT-4/src/WordStatCount.java
+++ HT-4/src/WordStatCount.java
@@ -8,74 +8,6 @@ import java.util.List;
 import java.util.function.IntPredicate;
 
 
-/*
-Fix:
-- double try;
-- killing JVM (exit)
-- errors to System.err
-- double memory in some cases => call two variants: fast and without memory overhead
- */
-
-class WordByWordReader {
-    private BufferedReader buffReader;
-    private List<String> tempWords;
-
-
-    private List<String> splitBy(String stringToSplit, IntPredicate isSpace) {
-        List<String> result = new ArrayList<>();
-
-        int n = stringToSplit.length();
-        for (int wordStart = 0, wordEnd = 0; wordEnd <= n; wordEnd++) {
-            if (wordEnd == n || isSpace.test(stringToSplit.charAt(wordEnd))) {
-                if (wordEnd > wordStart) {
-                    result.add(stringToSplit.substring(wordStart, wordEnd));
-                }
-                wordStart = wordEnd + 1;
-            }
-        }
-
-        return result;
-    }
-
-    private void resupplyTemp() throws IOException {
-        String nextLine = null;
-        while (tempWords.isEmpty()) {
-            nextLine = buffReader.readLine();
-            if (nextLine == null) {
-                return;
-            }
-
-            tempWords = splitBy(nextLine, (int boxed) -> {
-                char ch = (char)boxed;
-                return !(Character.isLetter(ch) || Character.getType(ch) == Character.DASH_PUNCTUATION || ch == '\'');
-            }
-            );
-            Collections.reverse(tempWords);
-
-        }
-    }
-
-
-
-    WordByWordReader(BufferedReader reader) throws IOException {
-        buffReader = reader;
-        tempWords = new ArrayList<String>();
-        resupplyTemp();
-    }
-
-    public boolean hasNextWord() {
-        return !tempWords.isEmpty();
-    }
-
-    public String nextWord() throws IOException {
-        String result = tempWords.get(tempWords.size() - 1);
-        tempWords.remove(tempWords.size() - 1);
-        resupplyTemp();
-
-        return result;
-    }
-}
-
 class WordWithCount {
     String word;
     int count;
@@ -87,12 +19,65 @@ class WordWithCount {
 }
 
 public class WordStatCount {
-    private static LinkedHashMap<String, Integer> constructCounter(BufferedReader buffReader) throws IOException {
-        LinkedHashMap<String, Integer> counter = new LinkedHashMap<>();
-        WordByWordReader splitter = new WordByWordReader(buffReader);
+    public static void main(String[] args) {
+        String inputFilename = args[0];
+        String outputFilename = args[1];
 
-        while(splitter.hasNextWord()) {
-            String thisWord = splitter.nextWord().toLowerCase();
+        LinkedHashMap<String, Integer> counter = new LinkedHashMap<>(); // accessOrder = false by default
+
+        // Read input file with try-resource construction:
+        try (InputStreamReader inputFileReader = new InputStreamReader(
+            new FileInputStream(inputFilename),
+            StandardCharsets.UTF_8
+        )
+        ) {
+            counter = constructCounter(inputFileReader);
+        } catch (FileNotFoundException e) {
+            System.err.println("No such file (provided as input): " + inputFilename);
+            return;
+        } catch (IOException e) {
+            System.err.println("There were some errors while working with input file");
+        }
+
+        List<WordWithCount> wordStat =
+//            dumpCounterToListFast(counter)
+            dumpCounterToListMemoryEfficient(counter)
+            ;
+
+        // This is stable and O(n log n) in worst case
+        Collections.sort(wordStat, Comparator.comparingInt(c -> c.count));
+
+
+        // Write on output file with try-resource construction:
+        try (BufferedWriter writer = new BufferedWriter(
+            new OutputStreamWriter(
+                new FileOutputStream(outputFilename),
+                StandardCharsets.UTF_8
+            )
+        )) {
+            writeWordsWithCounts(wordStat, writer);
+        } catch (FileNotFoundException e) {
+            System.err.println("FileNotFoundException appeared => the file exists but is a directory"
+                +"rather than a regular file, does not exist but cannot"
+                +"be created, or cannot be opened for any other reason");
+            return;
+        } catch (IOException e) {
+            System.err.println("There were some errors while working with output file");
+            return;
+        }
+    }
+
+
+    private static LinkedHashMap<String, Integer> constructCounter(InputStreamReader reader) throws IOException {
+        LinkedHashMap<String, Integer> counter = new LinkedHashMap<>();
+        BufferedScanner splitter = new BufferedScanner(reader);
+
+        while(true) {
+            String thisWord = splitter.nextWord();
+            if (thisWord == null){
+                break;
+            }
+            thisWord = thisWord.toLowerCase();
             counter.put(thisWord, counter.getOrDefault(thisWord, 0) + 1);
         }
 
@@ -121,48 +106,5 @@ public class WordStatCount {
             head.remove();
         }
         return wordStat;
-    }
-
-    public static void main(String[] args) {
-        String inputFilename = args[0];
-        String outputFilename = args[1];
-
-        LinkedHashMap<String, Integer> counter = new LinkedHashMap<>(); // accessOrder = false by default
-
-        // Read input file with try-resource idiom:
-
-        try (BufferedReader buffReader = new BufferedReader(
-            new InputStreamReader(
-                new FileInputStream(inputFilename),
-                StandardCharsets.UTF_8
-            ))
-        ) {
-            counter = constructCounter(buffReader);
-        } catch (FileNotFoundException e) {
-            System.err.println("No such file (provided as input): " + inputFilename);
-            return;
-        } catch (IOException e) {
-            System.err.println("There were some errors while working with input file");
-        }
-
-        List<WordWithCount> wordStat =
-//            dumpCounterToListFast(counter)
-            dumpCounterToListMemoryEfficient(counter)
-            ;
-        // This is stable and O(n log n) in worst case
-        Collections.sort(wordStat, Comparator.comparingInt(c -> c.count));
-
-        // Write on output file with try-resource idiom:
-        try (BufferedWriter writer = new BufferedWriter(
-            new OutputStreamWriter(
-                new FileOutputStream(outputFilename),
-                StandardCharsets.UTF_8
-            )
-        )) {
-            writeWordsWithCounts(wordStat, writer);
-        } catch (IOException e) {
-            System.err.println("There were some errors while working with output file");
-            return;
-        }
     }
 }
