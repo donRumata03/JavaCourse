1863922bba8091f0e31ee133f16dff189b05b1fe
donRumata <donrumata03@gmail.com>
2021-11-15 21:25:04 +0300

    Add «code» inline markup element

---
+++ markup/Code.java
@@ -0,0 +1,20 @@
+package markup;
+
+import java.util.List;
+
+public class Code extends WrappingMarkupElement implements InlineMarkupElement {
+
+    public Code(List<InlineMarkupElement> children) {
+        super((List<MarkupElement>)(List<? extends MarkupElement>) children);
+    }
+
+    @Override
+    protected String getMarkdownDelimiter() {
+        return "`";
+    }
+
+    @Override
+    protected String getHtmlDelimiter() {
+        return "code";
+    }
+}
