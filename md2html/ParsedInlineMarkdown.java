8702299a6258470ba6a477943818beaaa6d22cbe
donRumata <donrumata03@gmail.com>
2021-11-16 12:34:23 +0300

    Temp struct for generalized inline markdown parsing representation

---
+++ md2html/ParsedInlineMarkdown.java
@@ -0,0 +1,9 @@
+package md2html;
+
+import java.util.List;
+import markup.InlineMarkupElement;
+import markup.MarkupElement;
+
+public class ParsedInlineMarkdown {
+    public List<InlineMarkupElement> children;
+}
