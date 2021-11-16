b2934a2cf327841480fbc22d1b08b7cf5b93f80e
donRumata <donrumata03@gmail.com>
2021-11-16 12:52:37 +0300

    Implement next block

--- md2html/MarkdownBlockTokenizer.java
+++ md2html/MarkdownBlockTokenizer.java
@@ -3,6 +3,8 @@ package md2html;
 import HT_5.BufferedScanner;
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class MarkdownBlockTokenizer implements Closeable, AutoCloseable {
@@ -12,9 +14,22 @@ public class MarkdownBlockTokenizer implements Closeable, AutoCloseable {
         in = bufferedScanner;
     }
 
-    Optional<String> nextBlock() {
-        // TODO
-        return Optional.empty();
+    Optional<String> nextBlock() throws IOException {
+        if (!in.hasNextChar()) {
+            return Optional.empty();
+        }
+
+        List<String> lines = new ArrayList<>();
+
+        // Split here by empty lines:
+        while (true) {
+            String nextLine = in.nextLine();
+            if (nextLine.equals("")) {
+                break;
+            }
+            lines.add(nextLine);
+        }
+        return Optional.of(String.join("\n", lines));
     }
 
     @Override
