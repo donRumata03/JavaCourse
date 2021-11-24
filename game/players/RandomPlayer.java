e30c5ef03901a725690dcf24dc044d6d06ec06c4
donRumata03 <donrumata03@gmail.com>
2021-11-24 17:45:57 +0300

    Adapt RandomPlayer for new interface

--- game/players/RandomPlayer.java
+++ game/players/RandomPlayer.java
@@ -1,7 +1,9 @@
 package game.players;
 
+import game.CellState;
 import game.Discrete2dMove;
 import game.Player;
+import game.Position2d;
 import game.UnmodifiableBoardView;
 import java.util.Random;
 
@@ -11,13 +13,12 @@ public class RandomPlayer implements Player {
     @Override
     public Discrete2dMove makeMove(UnmodifiableBoardView view) {
         while (true) {
-            final Discrete2dMove move = new Discrete2dMove(
+            final Position2d position = new Position2d(
                     random.nextInt(3),
-                    random.nextInt(3),
-                    view.getNextPlayerIndex()
+                    random.nextInt(3)
             );
-            if (view.isValid(move)) {
-                return move;
+            if (view.cellStateAt(position) == CellState.E) {
+                return new Discrete2dMove(position);
             }
         }
     }
