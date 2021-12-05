8359244a2aa1ee65d94310ebc21347a7e8207adc
donRumata <donrumata03@gmail.com>
2021-12-05 22:29:44 +0300

    Add expecting mechanism for ParsableSource

--- src/expression/parser/generic/ParsableSource.java
+++ src/expression/parser/generic/ParsableSource.java
@@ -25,12 +25,27 @@ public class ParsableSource {
         return in.testNext(predicate);
     }
 
-    public boolean consumeIfExpected(char expected) throws IOException {
-        if (viewChar() == expected) {
+    public boolean testNextCharIs(char forTest) throws IOException {
+        return testNextChar(ch -> ch == forTest);
+    }
+
+    public boolean isEof() throws IOException {
+        return !in.hasNextChar();
+    }
+
+    // Methods under this boundary don't directly work with «in» and «nextPos»
+
+
+    public boolean consumeIf(IntPredicate predicate) throws IOException {
+        if (testNextChar(predicate)) {
             consumeChar();
             return true;
         }
         return false;
+    }
+
+    public boolean consumeIfExpected(char expected) throws IOException {
+        return consumeIf(ch -> ch == expected);
     }
 
     public boolean nextIsBetweenInclusive(final char from, final char to) throws IOException {
@@ -39,5 +54,55 @@ public class ParsableSource {
 
     public int getNextPos() {
         return nextPos;
+    }
+
+    public IllegalArgumentException generateUnexpectedCharacterError(char expected) throws IOException {
+        return new IllegalArgumentException(
+            String.format(
+                "Unexpected character at index %d: „%c” (expected: „%c”). Note that nobody expects Spanish Inquisition!",
+                nextPos, viewChar(), expected
+            )
+        );
+    }
+
+
+    public IllegalArgumentException generateUnexpectedCharacters(char expected) throws IOException {
+        return new IllegalArgumentException(
+            String.format(
+                "Unexpected character at index %d: „%c” (expected: „%c”). Note that nobody expects Spanish Inquisition!",
+                nextPos, viewChar(), expected
+            )
+        );
+    }
+
+    public void expectChar(char expected) throws IOException {
+        if (!consumeIfExpected(expected)) {
+            throw generateUnexpectedCharacterError(expected);
+        }
+    }
+
+    public void expectString(String expectedString) throws IOException {
+        for (final char c : expectedString.toCharArray()) {
+            expectChar(c);
+        }
+    }
+
+    public boolean expectStringIfFirstCharacterMatches(String expectedString) throws IOException {
+        if (testNextCharIs(expectedString.charAt(0))) {
+            int problematicShift = 0;
+            while (problematicShift < expectedString.length() && consumeIfExpected(expectedString.charAt(problematicShift))) {
+                problematicShift++;
+            }
+            if (problematicShift < expectedString.length()) {
+                // Expected string is started, but it doesn't match ==> Throw exception
+                throw new IllegalArgumentException(String.format(
+                    "String „%s” is detected to start but input doesn't match is: „…%s…”. Problematic position is: %d",
+                    expectedString, expectedString.substring(0, problematicShift) + viewChar(), nextPos
+                ));
+            }
+
+            return true;
+        }
+        return false;
     }
 }
