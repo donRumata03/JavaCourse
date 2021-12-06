bf7a720a501e14d20d1c2f0d0b0cc26c348adb8a
donRumata <donrumata03@gmail.com>
2021-12-07 00:44:39 +0300

    Adapt grammar for LL(1) parser

--- src/expression/parser/ExpressionParser.java
+++ src/expression/parser/ExpressionParser.java
@@ -1,11 +1,26 @@
 package expression.parser;
 
+import bufferedScanning.ReaderBufferizer;
 import expression.TripleExpression;
+import expression.parser.generic.ArithmeticExpressionTokenizer;
+import expression.parser.generic.ParsableSource;
+import java.io.StringReader;
 
 public class ExpressionParser implements Parser {
 
     @Override
     public TripleExpression parse(String expression) {
+        return ExpressionParser.parseExpression(
+            new ArithmeticExpressionTokenizer(
+                new ParsableSource(new ReaderBufferizer(new StringReader(expression)))
+            ));
+    }
+
+    public static TripleExpression parseExpression(ArithmeticExpressionTokenizer tokenizer) {
+        return null;
+    }
+
+    public static TripleExpression parseTerm(ArithmeticExpressionTokenizer tokenizer) {
         return null;
     }
 }
