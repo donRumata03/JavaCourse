e3aefd0f8c0c6689567e6d7d3edc5636987f0ed6
donRumata <donrumata03@gmail.com>
2021-12-14 11:29:30 +0300

    Add shift operations

---
+++ src/expression/ShiftLeft.java
@@ -0,0 +1,30 @@
+package expression;
+
+import expression.generic.OperatorTraits;
+import expression.generic.ParenthesesTrackingExpression;
+import expression.generic.TwoArgumentExpression;
+import java.math.BigDecimal;
+
+public class ShiftLeft extends TwoArgumentExpression {
+
+    public static final OperatorTraits OPERATOR_INFO = new OperatorTraits(
+        1,
+        false,
+        false,
+        "<<"
+    );
+
+    public ShiftLeft(ParenthesesTrackingExpression left, ParenthesesTrackingExpression right) {
+        super(left, right, OPERATOR_INFO);
+    }
+
+    @Override
+    public int reductionOperation(int leftResult, int rightResult) {
+        return leftResult << rightResult;
+    }
+
+    @Override
+    public BigDecimal reductionOperation(BigDecimal leftResult, BigDecimal rightResult) {
+        throw new RuntimeException("Only int supported");
+    }
+}
